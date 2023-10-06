93270e0a66bb9eb5cc76bcd64a71e3b728a83f03
Randy Moya <stevenmoya02@gmail.com>
2023-10-06 11:20:35 -0600

    Se creo un middleware para agregar la fecha actual a las peticiones

---
+++ Middlewares/TimeMiddleware.cs
@@ -0,0 +1,39 @@
+﻿namespace wepAPI.Middlewares
+{
+    public class TimeMiddleware
+    {
+        readonly RequestDelegate next;
+        public TimeMiddleware(RequestDelegate nextRequest)
+        {
+            next = nextRequest;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+
+            if (context.Request.Query.Any(p => p.Key == "time"))
+            {
+                await context.Response.WriteAsync(DateTime.Now.ToShortDateString());
+            }
+
+            if (!context.Response.HasStarted)
+            {
+                await next.Invoke(context);
+            }
+            
+
+        }
+
+
+    }
+
+    public static class TimeMiddlewareExtention
+    {
+        public static IApplicationBuilder UseTimeMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<TimeMiddleware>();
+        }
+    }
+}
+
+
