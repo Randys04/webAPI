a93e63b8968f82e72a64b415e0d160df539d474e
Randy Moya <stevenmoya02@gmail.com>
2023-10-22 19:11:50 -0600

    Se agregaron los servicios(Get, Save, Update y Delete) para cada modelo

---
+++ Services/CategoryService.cs
@@ -0,0 +1,62 @@
+﻿
+using tasks_Project;
+using tasks_Project.Models;
+
+namespace wepAPI.Services
+{
+    public class CategoryService
+    {
+        TasksContext context;
+
+        public CategoryService(TasksContext dbcontext)
+        {
+            context = dbcontext;
+        }
+
+        public IEnumerable<Category> Get()
+        {
+            return context.Categories;
+        }
+
+        public async System.Threading.Tasks.Task Save(Category category) 
+        {
+            context.Add(category);
+            await context.SaveChangesAsync();
+        }
+
+        public async System.Threading.Tasks.Task Update(Guid id,Category category)
+        {
+            var updatedCategory = context.Categories.Find(id);
+
+            if (updatedCategory != null)
+            {
+                updatedCategory.Name = category.Name;
+                updatedCategory.Description = category.Description;
+                updatedCategory.Weight = category.Weight;
+
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async System.Threading.Tasks.Task Delete(Guid id, Category category)
+        {
+            var deletedCategory = context.Categories.Find(id);
+
+            if (deletedCategory != null)
+            {
+                context.Remove(deletedCategory);
+
+                await context.SaveChangesAsync();
+            }
+        }
+
+    }
+
+    public interface ICategoryService 
+    {
+        IEnumerable<Category> Get();
+        System.Threading.Tasks.Task Save(Category category);
+        System.Threading.Tasks.Task Update(Guid id, Category category);
+        System.Threading.Tasks.Task Delete(Guid id, Category category);
+    }
+}
---
+++ Services/TaskService.cs
@@ -0,0 +1,61 @@
+﻿using tasks_Project;
+using tasks_Project.Models;
+using Task = tasks_Project.Models.Task;
+
+namespace wepAPI.Services
+{
+    public class TaskService
+    {
+        TasksContext context;
+
+        public TaskService(TasksContext dbcontext)
+        {
+            context = dbcontext;
+        }
+
+        public IEnumerable<Task> Get()
+        {
+            return context.Tasks;
+        }
+
+        public async System.Threading.Tasks.Task Save(Task task)
+        {
+            context.Add(task);
+            await context.SaveChangesAsync();
+        }
+
+        public async System.Threading.Tasks.Task Update(Guid id, Task task)
+        {
+            var updatedTask = context.Tasks.Find(id);
+
+            if (updatedTask != null)
+            {
+                updatedTask.Title = task.Title;
+                updatedTask.Description = task.Description;
+                updatedTask.TaskPriority = task.TaskPriority;
+                updatedTask.Summary = task.Summary;
+
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async System.Threading.Tasks.Task Delete(Guid id, Task task) 
+        {
+            var deletedTask = context.Tasks.Find(id);
+
+            if (deletedTask != null)
+            {
+                context.Remove(deletedTask);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+
+    public interface ITaskService
+    {
+        IEnumerable<Task> Get();
+        System.Threading.Tasks.Task Save(Task task);
+        System.Threading.Tasks.Task Update(Guid id, Task task);
+        System.Threading.Tasks.Task Delete(Guid id, Task task);
+    }
+}
